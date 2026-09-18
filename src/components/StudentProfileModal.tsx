cb5b81e0275f70a4a828a23facd87057a3f3a008
Zakaria Dou <douzakaria1998@gmail.com>
2026-09-18 17:35:28 +0100

    fix: remove duplicate StudentRecord import in StudentProfileModal

--- src/components/StudentProfileModal.tsx
+++ src/components/StudentProfileModal.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState } from 'react';
-import { StudentRecord, GroupSheet } from '../types';
 import { useApp, calcStudentFinancesPure } from '../context/AppContext';
 import {
   X,
